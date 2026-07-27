9170b40662101605a7e8b4ccad661ea01f11e77a
Joshua Kunst <jbkunst@gmail.com>
2026-07-27 00:03:02 -0400

    Use zero-padding card wrapper

--- kmeans/app.R
+++ kmeans/app.R
@@ -15,7 +15,7 @@ thematic::thematic_shiny(font = "auto")
 apptheme <- bs_theme()
 
 sidebar <- purrr::partial(bslib::sidebar, width = 300)
-card <- purrr::partial(bslib::card, full_screen = TRUE)
+card <- purrr::partial(bslib::card, full_screen = TRUE, wrapper = purrr::partial(bslib::card_body, padding = 0))
 
 primary_color <- unname(bs_get_variables(apptheme, c("primary")))
 
